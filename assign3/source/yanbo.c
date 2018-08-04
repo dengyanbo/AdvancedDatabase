6133bde106abf96e7dadaf604230ec5347286ed9
Yanbo's Ubuntu <dengyanbodyb@gmail.com>
2018-08-04 11:00:38 -0500

    start yanbo's part

--- assign3/source/yanbo.c
+++ assign3/source/yanbo.c
@@ -1,12 +1,18 @@
 #include "record_mgr.h"
 
 // handling records in a table
-extern RC insertRecord (RM_TableData *rel, Record *record);
-extern RC deleteRecord (RM_TableData *rel, RID id);
-extern RC updateRecord (RM_TableData *rel, Record *record);
-extern RC getRecord (RM_TableData *rel, RID id, Record *record);
+RC insertRecord (RM_TableData *rel, Record *record){
+    if(rel == NULL) return RC_RM_NO_MORE_TUPLES;
+    if(record == NULL) return RC_OK;
+    
+
+    return RC_OK;
+}
+RC deleteRecord (RM_TableData *rel, RID id);
+RC updateRecord (RM_TableData *rel, Record *record);
+RC getRecord (RM_TableData *rel, RID id, Record *record);
 
 // scans
-extern RC startScan (RM_TableData *rel, RM_ScanHandle *scan, Expr *cond);
-extern RC next (RM_ScanHandle *scan, Record *record);
-extern RC closeScan (RM_ScanHandle *scan);
+RC startScan (RM_TableData *rel, RM_ScanHandle *scan, Expr *cond);
+RC next (RM_ScanHandle *scan, Record *record);
+RC closeScan (RM_ScanHandle *scan);
